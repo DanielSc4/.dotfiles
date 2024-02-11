8026dde105ce7f145900cd6b30e62d3ef72cb57d
DanielSc4 <scalena99@gmail.com>
2024-02-12 00:08:22 +0100

    done with lsp

--- .config/nvim/lua/daniel/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/daniel/plugins/nvim-treesitter.lua
@@ -1,11 +1,12 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+            ensure_installed = { 'lua', 'python', 'rust',  'vimdoc', 'vim', 'bash' },
+            auto_install = true,
             sync_install = false,
             highlight = {enable = true},
             indent = {enable = true},
