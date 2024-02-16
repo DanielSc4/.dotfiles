978d254b09fa861d8623b0232ba90d0f8ac417eb
DanielSc4 <scalena99@gmail.com>
2024-02-16 19:58:03 +0100

    added few plugins

--- .config/nvim/lua/daniel/plugins/telescope.lua
+++ .config/nvim/lua/daniel/plugins/telescope.lua
@@ -2,13 +2,21 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-file-browser.nvim',
+        },
         config = function()
+            local telescope = require("telescope")
             local builtin = require('telescope/builtin')
+
+            telescope.load_extension("file_browser")
+            
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>fd', ":Telescope file_browser<cr><esc>", {})
         end,
     },
     {
