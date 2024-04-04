50f57e63d7b52ab426a3ef5b2162f88060c864dc
DanielSc4 <scalena99@gmail.com>
2024-04-04 15:59:31 +0200

    switch to normal mode when closing cmp window

--- .config/nvim/lua/daniel/plugins/nvim-cmp.lua
+++ .config/nvim/lua/daniel/plugins/nvim-cmp.lua
@@ -39,8 +39,17 @@ return {
                 ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                -- ["<C-e>"] = cmp.mapping.abort(),    -- close completion window
-                ["<Esc>"] = cmp.mapping.abort(),    -- close completion window
+                ["<C-e>"] = cmp.mapping.abort(),    -- close completion window
+                -- ["<Esc>"] = cmp.mapping.abort(),
+                ["<Esc>"] = cmp.mapping(function (fallback)
+                    -- abort and esc to normal mode
+                    if cmp.visible() then
+                        cmp.abort()
+                        fallback()
+                    else
+                        fallback()
+                    end
+                end),
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
